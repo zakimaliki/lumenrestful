5be213d1c8490a8f2ea0d8c98de7ebaffd9b9a43
zaki maliki <malikizakitop@gmail.com>
2020-07-01 17:37:43 +0700

    first commit

---
+++ app/Produk.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Produk extends Model  
+{
+    protected $table = 'produk';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nama', 'harga', 'warna',
+         'kondisi', 'diskripsi'
+    ];
+}
